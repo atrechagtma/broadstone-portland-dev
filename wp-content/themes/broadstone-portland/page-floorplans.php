ee8aee2c0fb486cbd38a61f71c352f0105855771
Andrew <atrecha@gtma.agency>
2022-02-22 13:56:18 -0800

    initial commit - new local build copy of bsp-staging wordkeeper site

---
+++ wp-content/themes/broadstone-portland/page-floorplans.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Floorplans Page Template
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package gutenberg-starter-theme
+ */
+
+get_header(); ?>
+<header class="entry-header" style="background-image: url(https://bsp-staging.local/wp-content/uploads/2021/11/Broadstone-Portland-detail25.jpg);">
+    <h1 class="entry-title">Floor Plans</h1>
+</header>
+<?php echo do_shortcode( '[floorplan_grid property_code="4853343"]' ); ?>
+<?php
+get_footer();
