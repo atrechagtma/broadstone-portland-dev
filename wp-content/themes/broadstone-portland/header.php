c94a4840d45fa2448b005c6410e12744f0393263
Andrew <atrecha@gtma.agency>
2022-07-01 15:45:37 -0700

    Remove google tag GTM-TSZ4J32 from header.php

--- wp-content/themes/broadstone-portland/header.php
+++ wp-content/themes/broadstone-portland/header.php
@@ -32,15 +32,6 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-K99XKFX');</script>
     <!-- End Google Tag Manager -->
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TSZ4J32');</script>
-    <!-- End Google Tag Manager -->
-
     <?php wp_head(); ?>
 </head>
 
@@ -49,10 +40,6 @@
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K99XKFX"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSZ4J32"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) --> 
 
     <div id="page" class="site">
         <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'gutenberg-starter-theme'); ?></a>
